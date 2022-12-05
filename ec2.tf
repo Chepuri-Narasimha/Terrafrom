b1d58411e7b51ffbe8d68f90d0ba3857811cf24b
Chepuri-Narasimha <Chepuri.bala208@gmail.com>
2022-12-05 16:53:15 +0530

    12/5

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
-/*resource "aws_ec2_instance" "myec2" {
+/*resource "aws_instance" "myec2" {
     ami = "ami-074dc0a6f6c764218"
-    instnace_type = "t2.micro"  
+      instance_type = "t2.micro"  
 }
 */
 
