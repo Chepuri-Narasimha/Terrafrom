d2d77d87c593f1d82271caf9a50e5faf10634ac1
Chepuri Bala Narasimha <bala@Jayasimhas-MacBook-Pro.local>
2024-06-06 10:41:29 +0530

    test1

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_ec2_instance" "myec2" {
+resource "aws_instance" "myec2" {
     ami = "ami-0163d8bb0e1bc3cb4"
-    instnace_type = "t2.micro"  
+    instance_type = "t2.micro"  
     tags = {
       name = "TFC-SERVER"
     }
